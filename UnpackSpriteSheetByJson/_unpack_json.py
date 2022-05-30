2260165317957d9aabfd037ec03a9022f60dd670
joyjjjjz <joyjjjz@126.com>
2022-05-30 20:59:44 +0800

    还原界面图集、动画图集：支持sourceW、sourceH

--- UnpackSpriteSheetByJson/_unpack_json.py
+++ UnpackSpriteSheetByJson/_unpack_json.py
@@ -28,6 +28,10 @@ def json_to_dict(json_filename):
         one_item['y'] = one_json_item['y']
         one_item['w'] = one_json_item['w']
         one_item['h'] = one_json_item['h']
+        if "sourceW" in one_json_item:
+            one_item["sourceW"] = one_json_item["sourceW"]
+        if "sourceH" in one_json_item:
+            one_item["sourceH"] = one_json_item["sourceH"]
         all_item_list.append(one_item)
 
     return all_item_list
@@ -55,7 +59,15 @@ def gen_png_from_json(folder_name, json_filename, png_filename):
         #设置图像裁剪区域 (x左上，y左上，x右下,y右下)
         image_box = [x, y, x + w , y + h ]
         one_pic = big_image.crop(image_box)
-
+        newW = one_item_data.get("sourceW", 0)
+        newH = one_item_data.get("sourceH", 0)
+        if newW > 0 and newH > 0 and (w != newW or h != newH):
+            newImage = Image.open("di.png")
+            newImage = newImage.crop([0, 0, newW, newH])
+            offsetX = (newW - w) / 2
+            offsetY= (newH - h) / 2
+            newImage.paste(one_pic, (offsetX, offsetY))
+            one_pic = newImage
         one_pic.save(folder_name + "/" + file_name + '.png') # 存储裁剪得到的图像
         
         #print one_item_data
@@ -77,6 +89,7 @@ def GetMergeFiles(srcDir, dstFiles):
 if __name__ == '__main__':
     # rootdir = sys.argv[1]
     rootdir = "D:/unpack_res"
+    rootdir = "D:/unpack_res/resource/image/public"
     #'E:/_github/Python/TexturePacker'
 
     files_set = set()
